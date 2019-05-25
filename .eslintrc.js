174a41e66092fe4b857c0f570ff390a8a79da583
yuki <yk.yuki21@gmail.com>
2019-05-25 17:19:33 +0900

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,31 @@
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    node: true
+  },
+  parserOptions: {
+    parser: 'babel-eslint'
+  },
+  extends: [
+    '@nuxtjs',
+    'plugin:nuxt/recommended',
+    'plugin:prettier/recommended',
+    'prettier',
+    'prettier/vue'
+  ],
+  plugins: [
+    'prettier'
+  ],
+  // add your custom rules here
+  rules: {
+    'no-console': 'off',
+    'prettier/prettier': [
+      'error',
+      {
+        'printWidth': 120,
+        'endOfLine': 'auto'
+      }
+    ]
+  }
+}
