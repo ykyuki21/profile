a263a3e1a8853d0095784faaf8ca016b78cf3280
yuki <yk.yuki21@gmail.com>
2019-05-26 14:45:32 +0900

    Fix default layout

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
-  <v-app dark>
-    <v-navigation-drawer v-model="drawer" :mini-variant="miniVariant" :clipped="clipped" fixed app>
+  <v-app>
+    <v-navigation-drawer v-model="drawer" class="hidden-sm-and-up" fixed app temporary>
       <v-list>
         <v-list-tile v-for="(item, i) in items" :key="i" :to="item.to" router exact>
           <v-list-tile-action>
@@ -12,40 +12,25 @@
         </v-list-tile>
       </v-list>
     </v-navigation-drawer>
-    <v-toolbar :clipped-left="clipped" fixed app>
-      <v-toolbar-side-icon @click="drawer = !drawer" />
-      <v-btn icon @click.stop="miniVariant = !miniVariant">
-        <v-icon>{{ `chevron_${miniVariant ? 'right' : 'left'}` }}</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="clipped = !clipped">
-        <v-icon>web</v-icon>
-      </v-btn>
-      <v-btn icon @click.stop="fixed = !fixed">
-        <v-icon>remove</v-icon>
-      </v-btn>
-      <v-toolbar-title v-text="title" />
+    <v-toolbar fixed app color="primary">
+      <v-toolbar-side-icon class="hidden-sm-and-up" @click="drawer = !drawer" />
+      <v-toolbar-title>
+        <nuxt-link to="/">{{ title }}</nuxt-link>
+      </v-toolbar-title>
       <v-spacer />
-      <v-btn icon @click.stop="rightDrawer = !rightDrawer">
-        <v-icon>menu</v-icon>
-      </v-btn>
+      <v-toolbar-items class="hidden-xs-only">
+        <v-btn v-for="(item, i) in items" :key="i" flat :to="item.to" router>{{ item.title }}</v-btn>
+      </v-toolbar-items>
     </v-toolbar>
     <v-content>
       <v-container>
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer v-model="rightDrawer" :right="right" temporary fixed>
-      <v-list>
-        <v-list-tile @click.native="right = !right">
-          <v-list-tile-action>
-            <v-icon light>compare_arrows</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Switch drawer (click me)</v-list-tile-title>
-        </v-list-tile>
-      </v-list>
-    </v-navigation-drawer>
-    <v-footer :fixed="fixed" app>
-      <span>&copy; 2019</span>
+    <v-footer fixed app color="primary">
+      <v-layout justify-center>
+        <span>&copy; 2019</span>
+      </v-layout>
     </v-footer>
   </v-app>
 </template>
@@ -54,26 +39,23 @@
 export default {
   data() {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
+      title: `Yuki's Portfolio`,
       items: [
-        {
-          icon: 'apps',
-          title: 'Welcome',
-          to: '/'
-        },
         {
           icon: 'bubble_chart',
           title: 'Inspire',
           to: '/inspire'
         }
       ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      drawer: false
     }
   }
 }
 </script>
+
+<style>
+.v-toolbar__title a {
+  color: black;
+  text-decoration: none;
+}
+</style>
