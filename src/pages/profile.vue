b611c3b0af3014735f9b6a970bef4197bd524fd2
yuki <yk.yuki21@gmail.com>
2019-05-31 23:41:29 +0900

    Fix profile

--- src/pages/profile.vue
+++ src/pages/profile.vue
@@ -1,7 +1,9 @@
 <template>
   <v-flex>
     <h2 class="headline">About</h2>
-    <p>兵庫県在住、神戸の街が好きな1991年5月生まれの28歳</p>
+    <p>兵庫県在住、神戸の街が好きな{{ getAge }}歳</p>
+    <p>お仕事ではECサイトのバックエンドを担当</p>
+    <p>一番幸福を感じる瞬間は美味しい物を食べている時</p>
     <p>趣味はスポーツ観戦、ゲーム</p>
     <p>主に観るスポーツはサッカー(Jリーグ)、League of Legends</p>
     <p>ゲームはメルクストーリアを中心にプレイ中</p>
@@ -33,6 +35,24 @@ export default {
     return {
       skills: ['C#', 'ASP.NET', 'CSS', 'JavaScript', 'PostgreSQL', 'IIS', 'SVN', 'Git', 'Jenkins'],
       hobby_skills: ['TypeScript', 'Vue.js', 'Firebase']
+    }
+  },
+  computed: {
+    getAge: function() {
+      const today = new Date()
+      const birth = new Date('1991-05-21')
+
+      // TODO: 今年の年齢を変数名でわかりやすく表現したい
+      const age = today.getFullYear() - birth.getFullYear()
+
+      if (
+        today.getMonth() > birth.getMonth() ||
+        (today.getMonth() === birth.getMonth() && today.getDate() >= birth.getDate())
+      ) {
+        return age
+      } else {
+        return age - 1
+      }
     }
   }
 }
